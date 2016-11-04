881a37292ab9c633ff8d41de6a5baa8446bd4525
Wei-ju Wu <weiju.wu@gmail.com>
2016-11-04 12:58:42 -0700

    integrated a template field and shortcode
    the content of a fake page is now determined by
    the page template, added a bicluster_genes shortcode
    to render the row members

--- biclusters.php
+++ biclusters.php
@@ -21,6 +21,8 @@ License URI: https://www.gnu.org/licenses/gpl-3.0.html
  **********************************************************************/
 
 function biclusters_settings_init() {
+
+    // This is the General section
     add_settings_section(
         "general_section",
         "Biclusters",
@@ -34,6 +36,17 @@ function biclusters_settings_init() {
 
     register_setting('general', 'source_url');
     register_setting('general', 'bicluster_slug');
+
+    // This is the writing section
+    add_settings_section(
+        "writing_section",
+        "Biclusters",
+        "writing_section_cb",
+        'writing'
+    );
+    add_settings_field('bicluster_info_template', 'Template for Bicluster Pages', 'info_template_field_cb', 'writing',
+                       'writing_section');
+    register_setting('writing', 'bicluster_info_template');
 }
 
 function general_section_cb()
@@ -53,6 +66,16 @@ function slug_field_cb()
     echo "<input type=\"text\" name=\"bicluster_slug\" value=\"" . $slug . "\">";
 }
 
+function writing_section_cb()
+{
+    echo "<p>General settings for the Biclusters Plugin</p>";
+}
+
+function info_template_field_cb()
+{
+    $info_template = get_option('bicluster_info_template', 'This is a test');
+    echo "<textarea rows=\"3\" cols=\"80\" name=\"bicluster_info_template\">" . $info_template . "</textarea>";
+}
 
 /**********************************************************************
  * Plugin Section
@@ -75,6 +98,22 @@ function demo_shortcode($atts=[], $content=null)
     return $content;
 }
 
+function bicluster_genes_shortcode($atts=[], $content=null)
+{
+    if ($content == null) {
+        $content = '';
+    }
+    $source_url = get_option('source_url', '');
+    $bicluster_num = get_query_var('bicluster');
+    $row_membs_json = file_get_contents($source_url . "/api/v1.0.0/cluster_genes/" . $bicluster_num);
+    $row_membs = json_decode($row_membs_json, true)["genes"];
+    foreach ($row_membs as $m) {
+        $content .= "<li>" . $m . "</li>";
+    }
+    $content .= "</ul>";
+    return $content;
+}
+
 /**********************************************************************
  * Custom post type (TODO)
  * We can define custom bicluster pages using a custom post type
@@ -88,11 +127,13 @@ function demo_shortcode($atts=[], $content=null)
  * TODO: can we have user-defined page templates for bicluster info pages ?
  * Example URL: http://localhost/~weiju/wordpress/index.php/bicluster/?bicluster=23
  **********************************************************************/
+
 function biclusters_fakepage_detect($posts)
 {
     global $wp, $wp_query;
     $plugin_slug =  get_option('bicluster_slug', 'biclusters');
     $source_url = get_option('source_url', '');
+    $content_template = get_option('bicluster_info_template');
 
     $url_comps = explode('/', $wp->request);
     $lead = $url_comps[0];
@@ -101,6 +142,7 @@ function biclusters_fakepage_detect($posts)
 
     if ($lead == $plugin_slug) {
 
+        /*
         $content = "<div>";
         $content .= "<h3>Row members</h3>";
         $row_membs_json = file_get_contents($source_url . "/api/v1.0.0/cluster_genes/" . $bicluster_num);
@@ -112,13 +154,14 @@ function biclusters_fakepage_detect($posts)
         $rm_list .= "</ul></p>";
         $content .= $rm_list;
         $content .= "</div>";
+        */
 
         error_log("FAKE PAGE DETECTOR EXECUTED for biclusters plugin");
         $post = new stdClass;
         $post->post_author = 1;
         $post->post_name = 'bicluster';
         $post->post_title = 'Information for Bicluster ' . $bicluster_num;
-        $post->post_content = $content;
+        $post->post_content = $content_template; //$content;
         $post->ID = -999;
         $post->post_type = 'page';
         $post->post_status = 'status';
@@ -159,6 +202,7 @@ function add_query_vars_filter($vars) {
 function biclusters_init()
 {
     add_shortcode('bicluster_demo', 'demo_shortcode');
+    add_shortcode('bicluster_genes', 'bicluster_genes_shortcode');
     add_filter('the_posts', 'biclusters_fakepage_detect');
     add_filter('query_vars', 'add_query_vars_filter');
 }
